e7bdb73642a2169ef750328e78fb00d57ee14300
Florian Bergmann <florian.bergmann@cpnv.ch>
2021-04-30 16:21:47 +0200

    add server and client

---
+++ client/main.js
@@ -0,0 +1,29 @@
+import {
+    client as WebSocketClient
+} from 'websocket';
+
+var client = new WebSocketClient();
+
+client.on('connectFailed', (error) => console.log('Connect Error: ' + error.toString()));
+
+client.on('connect', (connection) => {
+    console.log('WebSocket Client Connected');
+    connection.on('error', (error) => console.log("Connection Error: " + error.toString()));
+    connection.on('close', () => console.log('echo-protocol Connection Closed'));
+    connection.on('message', (message) => {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+
+    function sendNumber() {
+        if (connection.connected) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            connection.sendUTF(number.toString());
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    sendNumber();
+});
+
+client.connect('ws://localhost:8080/', 'echo-protocol');
---
+++ server/main.js
@@ -0,0 +1,50 @@
+var WebSocketServer = require('websocket').server;
+var WebSocketClient = require('websocket').client;
+var WebSocketFrame = require('websocket').frame;
+var WebSocketRouter = require('websocket').router;
+var W3CWebSocket = require('websocket').w3cwebsocket;
+var http = require('http');
+
+var server = http.createServer((request, response) => {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(8080, () => {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', (message) => {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', (reasonCode, description) => {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
